3cf7cecb7f090649e383871dfe599ff7c9b9a653
MateuszWiorek <38459815+MateuszWiorek@users.noreply.github.com>
2018-11-26 01:15:54 +0100

    Delete main.java

--- src/com/wiorek/src/main.java
+++
@@ -1,8 +0,0 @@
-package com.wiorek.src;
-
-public class main {
-    public static void main(String[]args) {
-        //example of using converting method
-        System.out.println(RomanNumerialsConvertion.convertIntToRoman(5)));
-    }
-}
